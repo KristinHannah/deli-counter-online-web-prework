e19f50b97fad7167fae1dd865465dff84d44b8bf
Kristin Kraemer <kristinhannahk@gmail.com>
2019-04-16 03:44:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    array.each do | item |
+    puts "The line is currently: 
+  end 
+end
+end 
