532433cb1ce29221b0854844e8637a00ea284fcf
Kristin Kraemer <kristinhannahk@gmail.com>
2019-04-16 03:57:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,11 +6,12 @@ def line(array)
   else 
     list =[]
     i = 1 
-    while i < array.length + 1 do 
-    list.push("#{i}. #{array[i - 1]} ")
+    while i < array.length do 
+    list.push("#{i}. #{array[i - 1]}")
     i += 1 
   end 
+  list.push("#{array.length}. #{array[last]}")
   needed_string = list.join
-  puts "The line is currently #{needed_string}"
+  puts "The line is currently #{final}"
 end
 end 
