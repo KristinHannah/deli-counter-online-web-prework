96b6d43e764dfd393c3519605b5a953f33b780b5
Kristin Kraemer <kristinhannahk@gmail.com>
2019-04-16 03:46:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,16 @@ def line(array)
   if array.length == 0 
     puts "The line is currently empty."
   else 
-    array.each do | item |
-    puts "The line is currently: 
+    list =[]
+    i = 1 
+    while i < array.length + 1 do 
+    list.push(#{i}. #{[array[i - 1]})
   end 
 end
 end 
+
+i = 1  
+while i < array.length + 1 do 
+  assignments.push("Hello, #{array[i - 1]}! You'll be assigned to room #{i}!")
+  i += 1 
+end
